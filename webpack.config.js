4d83741b8a13e73b217dba6b4f3a184a3c3fa151
winterandrea <andrecasero@gmail.com>
2020-06-21 13:24:25 +0200

    push

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "react-component-storybook.js",
+    library: "react-component-storybook",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugin: [new CleanWebpackPlugin(["dist"])],
+};
